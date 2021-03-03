35ffcba9d4e478b16c418697ec776ac26e18fa61
JeremieSD <jeremie9000@gmail.com>
2021-03-03 21:46:31 +0000

    Alrighty searches basic version should be done.

--- src/Backend/searchingFunctionality.js
+++ src/Backend/searchingFunctionality.js
@@ -3,11 +3,24 @@ const WIKIPEDIA_ENDPOINT_SEARCH = 'https://en.wikipedia.org/w/api.php';
 const MEDIAWIKI_ENDPOINT = 'https://www.mediawiki.org/w/api.php';
 const NUMBER_OF_RETRIES = 5;
 const fetch = require("node-fetch");
+var Feed = require('rss-to-json');
 
-
+//Send you all the information regarding a user
+//@params {name} - User name
+//@returns {json} - regardless of whether the user has done anything or not
+export const userSearch = async(name) =>{
+    const params ={
+        action: 'feedcontributions',
+        user: name
+    };
+    var item = await wikipediaQueryRSS(MEDIAWIKI_ENDPOINT,params,NUMBER_OF_RETRIES);
+    console.log(item);
+    return item;
+};
 //SuperFunction for page revisions search
-//@Param {string} searchitem - gets page revisions of search and returns -1 otherwise
-const pageRevisionsSearch = async(searchitem) =>{
+//@Param {string} searchitem - item to query for revisions
+//@returns {Promise} returns revisions if exists otherwise returns -1
+export const pageRevisionsSearch = async(searchitem) =>{
     var item = await (getWikibaseItem(searchitem));
     if (item == -1){
         return -1;
@@ -16,34 +29,6 @@ const pageRevisionsSearch = async(searchitem) =>{
     return item
 };
 
-//Grabs wikibase_id from a search
-//@param {string} searchItem - Item to search Qid for
-//@returns {Object} -1 or QID 
-const getWikibaseItem = async (searchItem) => {
-    const params = {
-        action: 'query',
-        format: 'json',
-        prop: 'pageprops',
-        titles: searchItem,
-    };
-    return await wikipediaQuery(WIKIPEDIA_ENDPOINT_SEARCH,params,NUMBER_OF_RETRIES).then(result=>extraResult(result.query.pages));
-};
-//Grabs revisions from qid, past 500 revisions only due to limitations from api
-// @param {string} qid - id to search revisions for 
-// @returns {Object} -1 or revisions in json
-const getRevisionsOfPage = async (qid) => {
-    const params = {
-        action: 'query',
-        format: 'json',
-        prop: 'revisions',
-        titles: qid,
-        rvprops: 'ids|timestamp|flags|comment|user',
-        rvlimit: 500,
-        rvdir: 'older'
-    };
-    var item = await wikipediaQuery(WIKIDATA_ENDPOINT,params,NUMBER_OF_RETRIES).then(result=>getRevisions(result.query.pages));
-    return item;
-};
 
 // ~ Helper Functions ---------------------------------------------------------
 
@@ -67,6 +52,25 @@ const wikipediaQuery = async (endpoint, params, n) => {
     }
   };
 
+/**
+ * Returns a the response of a query to the Mediawiki API endpoint
+ *
+ * @param {Object} params - Object of parameters to use when querying
+ * @param {number} n - Number of times to retry if failure occurs
+ * @returns {Promise.<Object>}
+ */
+const wikipediaQueryRSS = async (endpoint, params, n) => {
+    try {
+      const paramsString = new URLSearchParams(params).toString();
+      const url = endpoint + '?' + paramsString;
+      return await Feed.load(url);
+    } catch (err) {
+      if (n === 1) {
+        throw err;
+      }
+      return setTimeout(wikipediaQueryRSS(endpoint, params, n - 1), 500);
+    }
+  };
 // Helper function to check whether something is a json string
 // @Param {string} str - string to be checked to see if it is in valid json
 // @returns {boolean} - returns whether the string is in valid json format
@@ -104,4 +108,33 @@ const getRevisions = async (json1) =>{
     if (json1[temp].hasOwnProperty('revisions')){
         return json1[temp].revisions;
     };return -1;
+};
+
+//Grabs wikibase_id from a search
+//@param {string} searchItem - Item to search Qid for
+//@returns {Object} -1 or QID 
+const getWikibaseItem = async (searchItem) => {
+    const params = {
+        action: 'query',
+        format: 'json',
+        prop: 'pageprops',
+        titles: searchItem,
+    };
+    return await wikipediaQuery(WIKIPEDIA_ENDPOINT_SEARCH,params,NUMBER_OF_RETRIES).then(result=>extraResult(result.query.pages));
+};
+//Grabs revisions from qid, past 500 revisions only due to limitations from api
+// @param {string} qid - id to search revisions for 
+// @returns {Object} -1 or revisions in json
+const getRevisionsOfPage = async (qid) => {
+    const params = {
+        action: 'query',
+        format: 'json',
+        prop: 'revisions',
+        titles: qid,
+        rvprops: 'ids|timestamp|flags|comment|user',
+        rvlimit: 500,
+        rvdir: 'older'
+    };
+    var item = await wikipediaQuery(WIKIDATA_ENDPOINT,params,NUMBER_OF_RETRIES).then(result=>getRevisions(result.query.pages));
+    return item;
 };
