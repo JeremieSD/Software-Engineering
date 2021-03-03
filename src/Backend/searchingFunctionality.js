43e65d27d93e7a64a34dcc4bb5d0f88271cbf7df
JeremieSD <jeremie9000@gmail.com>
2021-03-03 17:38:39 +0000

    Page Revisions Search is done.

--- src/Backend/searchingFunctionality.js
+++ src/Backend/searchingFunctionality.js
@@ -4,21 +4,46 @@ const MEDIAWIKI_ENDPOINT = 'https://www.mediawiki.org/w/api.php';
 const NUMBER_OF_RETRIES = 5;
 const fetch = require("node-fetch");
 
+
+//SuperFunction for page revisions search
+//@Param {string} searchitem - gets page revisions of search and returns -1 otherwise
+const pageRevisionsSearch = async(searchitem) =>{
+    var item = await (getWikibaseItem(searchitem));
+    if (item == -1){
+        return -1;
+    }
+    item = await getRevisionsOfPage(item);
+    return item
+};
+
 //Grabs wikibase_id from a search
 //@param {string} searchItem - Item to search Qid for
 //@returns {Object} -1 or QID 
-export const getWikibaseItem = async (searchItem) => {
+const getWikibaseItem = async (searchItem) => {
     const params = {
         action: 'query',
         format: 'json',
         prop: 'pageprops',
         titles: searchItem,
     };
-    const item = wikipediaQuery(WIKIPEDIA_ENDPOINT_SEARCH,params,NUMBER_OF_RETRIES).then(result=>extraResult(result.query.pages));
+    return await wikipediaQuery(WIKIPEDIA_ENDPOINT_SEARCH,params,NUMBER_OF_RETRIES).then(result=>extraResult(result.query.pages));
+};
+//Grabs revisions from qid, past 500 revisions only due to limitations from api
+// @param {string} qid - id to search revisions for 
+// @returns {Object} -1 or revisions in json
+const getRevisionsOfPage = async (qid) => {
+    const params = {
+        action: 'query',
+        format: 'json',
+        prop: 'revisions',
+        titles: qid,
+        rvprops: 'ids|timestamp|flags|comment|user',
+        rvlimit: 500,
+        rvdir: 'older'
+    };
+    var item = await wikipediaQuery(WIKIDATA_ENDPOINT,params,NUMBER_OF_RETRIES).then(result=>getRevisions(result.query.pages));
     return item;
 };
-
-
 
 // ~ Helper Functions ---------------------------------------------------------
 
@@ -59,11 +84,24 @@ function isJson(str) {
 //@param {json} - json straight from query
 //@returns {promise} returns -1 if it fails to find it and otherwise returns wikibase_item 
 const extraResult = async (json1) =>{
-    var temp = 0;
+    var temp = -1;
     while (typeof json1[temp] == 'undefined'){
         temp = temp + 1;
     }
     if (json1[temp].hasOwnProperty('pageprops')){
         return json1[temp].pageprops.wikibase_item;
+    };return -1;
+};
+
+//Helper function to return revisions 
+//@param {json} - json straight from query
+//@returns {promise} returns -1 if it fails to find it and otherwise returns revisions
+const getRevisions = async (json1) =>{
+    var temp = 0;
+    while (typeof json1[temp] == 'undefined'){
+        temp = temp + 1;
+    }
+    if (json1[temp].hasOwnProperty('revisions')){
+        return json1[temp].revisions;
     };return -1;
 };
