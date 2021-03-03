9526984d14b42ff6790f166bdd7f1e6b374728e6
JeremieSD <jeremie9000@gmail.com>
2021-03-03 01:45:04 +0000

    This took way too long... gives back Q-id

--- src/Backend/searchingFunctionality.js
+++ src/Backend/searchingFunctionality.js
@@ -2,20 +2,27 @@ const WIKIDATA_ENDPOINT = 'https://www.wikidata.org/w/api.php';
 const WIKIPEDIA_ENDPOINT_SEARCH = 'https://en.wikipedia.org/w/api.php';
 const MEDIAWIKI_ENDPOINT = 'https://www.mediawiki.org/w/api.php';
 const NUMBER_OF_RETRIES = 5;
+const fetch = require("node-fetch");
 
 
-
-const getWikibaseItem = async searchItem => {
+const getWikibaseItem = async (searchItem) => {
     const params = {
         action: 'query',
         format: 'json',
         prop: 'pageprops',
         titles: searchItem,
     };
-    const item = wikipediaQuery(WIKIPEDIA_ENDPOINT_SEARCH,params,NUMBER_OF_RETRIES).then(result=>result.query.pages.pageprops.wikibase_item);
+    const item = wikipediaQuery(WIKIPEDIA_ENDPOINT_SEARCH,params,NUMBER_OF_RETRIES).then(result=>extraResult(result.query.pages));
     return item;
 };
-export default getWikibaseItem;
+
+const extraResult = async (json1) =>{
+    var temp = 0;
+    while (typeof json1[temp] == 'undefined'){
+        temp = temp + 1;
+    }
+    return json1[temp].pageprops.wikibase_item;
+};
 // ~ Helper Functions ---------------------------------------------------------
 
 /**
@@ -34,6 +41,17 @@ const wikipediaQuery = async (endpoint, params, n) => {
       if (n === 1) {
         throw err;
       }
-      return await setTimeout(wikipediaQuery(endpoint, params, n - 1), 500);
+      return setTimeout(wikipediaQuery(endpoint, params, n - 1), 500);
     }
   };
+
+function isJson(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
+
+getWikibaseItem("Albert Einstein").then((response) => console.log(response))
