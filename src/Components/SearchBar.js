2f9b7a0adcad9929323b18c7fa9c87949f1f9299
Matteo McGuinness <matteo.mcguinness@gmail.com>
2021-03-15 15:13:36 +0000

    'tempUpdate'

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,36 +1,101 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Component } from 'react';
+//import example from './example.json';
+class SearchBar extends Component {
+  // const [search, setSearch] = useState('');
+  // const [suggestions, setSuggestions] = useState([]);
 
-const SearchBar = () => {
-  const [search, setSearch] = useState('');
-  const [suggestions, setSuggestions] = useState([]);
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      loaded: false,
+    };
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (search) {
-      // return page user wants
-      console.log('searchValue = ' + search);
-      setSearch('');
-    } else {
-      // no search empty values
-      console.log('empty values');
-    }
+    this.loadData();
+  }
+
+  // useEffect(() => {
+  //   // Call function for suggestion based on search
+  //   if (search) {
+  //     console.log(search);
+  //     setSuggestions([]);
+  //     const values = example.query.prefixsearch;
+  //     values.map(obj => {
+  //       const title = obj.title;
+  //       const id = obj.pageid;
+  //       const sugg = { id, title };
+  //       setSuggestions(suggestions => {
+  //         return [...suggestions, sugg];
+  //       });
+  //     });
+  //     //console.log(search);
+  //   } else {
+  //     setSuggestions([]);
+  //   }
+  // }, [search]);
+
+  loadData = () => {
+    const getData = this.props.settings.getData.bind(this);
+    console.log('dataToBeFetched ');
+    getData().then(data => {
+      console.log('data ' + data);
+      // const smlData = data.slice(0, this.state.fullGraph ? 30 : 10);
+      // this.setState({
+      //   loaded: true,
+      //   data: smlData,
+      // });
+    });
   };
 
-  useEffect(() => {
-    // Call function for suggestion based on search
-    //const getSuggestions(search);
+  // refresh = async () => {
+  //   if (!this.props.paused) {
+  //     const method = this.props.settings.refreshMethod.bind(this);
+  //     await method();
+  //   }
+  // };
 
-    console.log(search);
-  }, [search]);
+  // componentDidMount = () => {
+  //   this.refreshInterval = setInterval(async () => {
+  //     await this.refresh();
+  //   }, this.props.settings.refreshTime);
+  // };
 
-  return (
-    <>
-      <article onSubmit={handleSubmit} style={{ height: '100%' }}>
-        <form>
+  handleSubmit = e => {
+    e.preventDefault();
+    // if (search) {
+    // return page user wants
+    //console.log('searchValue = ' + search);
+    // let item = getPrefixSearch('Star');
+
+    // item.then(function(result) {
+    //   console.log('R ' + result);
+    // });
+    // this.loadData();
+    // setSearch('');
+    // } else {
+    // no search empty values
+    //console.log('empty values');
+    // }
+  };
+
+  render() {
+    return (
+      <form
+        autoComplete="off"
+        onSubmit={this.handleSubmit}
+        style={{ height: '100%' }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            // margin: '2rem',
+          }}
+        >
           <div
+            className="autoComplete"
             style={{
-              display: 'flex',
-              flexDirection: 'row',
+              position: 'relative',
               margin: '2rem',
             }}
           >
@@ -38,16 +103,37 @@ const SearchBar = () => {
               type="text"
               id="input"
               name="input"
-              value={search}
-              onChange={e => setSearch(e.target.value)}
-              style={{ backgroundColor: '#D3D3D3', marginRight: '2rem' }}
-            />
-            <button type="submit">search</button>
+              // value={search}
+              // onChange={e => setSearch(e.target.value)}
+              style={{
+                backgroundColor: '#f1f1f1',
+                marginRight: '2rem',
+                width: '15rem',
+                borderStyle: 'solid',
+                borderColor: '#d4d4d4',
+              }}
+            ></input>
+            <input type="submit" style={{}} />
+
+            {/* {suggestions.map(suggestion => {
+              const { id, title } = suggestion;
+              return (
+                <div
+                  key={id}
+                  className="searchListSuggestion"
+                  // onClick={() => {
+                  //   setSearch(title);
+                  // }}
+                >
+                  {title}
+                </div>
+              );
+            })} */}
           </div>
-        </form>
-      </article>
-    </>
-  );
-};
+        </div>
+      </form>
+    );
+  }
+}
 
 export default SearchBar;
--- src/Pages/TestComponent.jsx
+++ src/Pages/TestComponent.jsx
@@ -1,17 +1,33 @@
-import { faFileExcel } from '@fortawesome/free-solid-svg-icons';
 import React, { Component } from 'react';
 import SearchBar from '../Components/SearchBar';
+import { pageRevisionsSearch } from '../Backend/searchingFunctionality';
+
+export const searchSuggestions = {
+  getData: async function() {
+    console.log('dataFetch');
+    return await pageRevisionsSearch('Star Wars');
+  },
+  // refreshTime: 2000,
+  // refreshMethod: function() {
+  //   this.loadData();
+  // },
+};
 
 class TestComponent extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { history: this.props.history };
+    this.state = { history: this.props.history, paused: false };
   }
+
+  handlePause = event => {
+    this.setState({ paused: event.target.value });
+  };
+
   render() {
     return (
       <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <SearchBar />
+        <SearchBar settings={searchSuggestions} paused={this.state.paused} />
       </div>
     );
   }
