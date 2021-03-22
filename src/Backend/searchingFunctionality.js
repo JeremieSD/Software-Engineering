080f284597cb58ed429daae9fd25ab099a56edda
JeremieSD <jeremie9000@gmail.com>
2021-03-22 22:10:39 +0000

    The rss issue should be fixed dont know if it actually works

--- src/Backend/searchingFunctionality.js
+++ src/Backend/searchingFunctionality.js
@@ -3,48 +3,51 @@ const WIKIPEDIA_ENDPOINT_SEARCH = 'https://en.wikipedia.org/w/api.php';
 const MEDIAWIKI_ENDPOINT = 'https://www.mediawiki.org/w/api.php';
 const NUMBER_OF_RETRIES = 5;
 const fetch = require('node-fetch');
-// var Feed = require('rss-to-json');
-
-//Send you all the information regarding a user
-//Returns whole User Query:
-//title: Think this is useless, havent explored too many of it though but it says that its user contributions and thats about it
-//link: link to user page
-//language: usually repersented like en
-//generator: no idea what this is
-//lastBuildDate:Last time it was queried
-//item: Array of items that the user edited
-//item.title: Title of what the edit was on
-//item.link: Link to the changes
-//item.guid: appears to be the same as link although further exploration is needed
-//item.description: Description of the change, can be used with NLP package to get links
-//item.pubDate: Format = Wed, 24 Feb 2021 01:22:25 GMT
-//item.dc:creator: Returns username of editor to item
-//item.comments: comment of the change
-//@params {name} - User name
-//@returns {json} - regardless of whether the user has done anything or not
-// export const userSearch = async name => {
-//   const params = {
-//     action: 'feedcontributions',
-//     user: name,
-//   };
-//   var item = await wikipediaQueryRSS(
-//     MEDIAWIKI_ENDPOINT,
-//     params,
-//     NUMBER_OF_RETRIES
-//   );
-//   return item;
-// };
-//SuperFunction for page revisions search
-//Returns Json array of revisions:
-//Each index will contain one revision which will all contain
-//revid: id for the revision that took place
-//parentid: id for the revision that was before it
-//user: user who did the revision
-//timestamp: yyyy-mm-ddT24hrZ format
-//comment: comment on whatever the user decided
-//@Param {string} searchitem - item to query for revisions
-//@returns {Promise} returns revisions if exists otherwise returns -1
-export const pageRevisionsSearch = async searchitem => {
+const $ = require('jquery');
+/** 
+  Send you all the information regarding a user
+  Returns whole User Query:
+  title: Think this is useless, havent explored too many of it though but it says that its user contributions and thats about it
+  link: link to user page
+  language: usually repersented like en
+  generator: no idea what this is
+  lastBuildDate:Last time it was queried
+  item: Array of items that the user edited
+  item.title: Title of what the edit was on
+  item.link: Link to the changes
+  item.guid: appears to be the same as link although further exploration is needed
+  item.description: Description of the change, can be used with NLP package to get links
+  item.pubDate: Format = Wed, 24 Feb 2021 01:22:25 GMT
+  item.dc:creator: Returns username of editor to item
+  item.comments: comment of the change
+  @params {name} - User name
+  @returns {json} - regardless of whether the user has done anything or not
+*/
+const userSearch = async name => {
+  const params = {
+    action: 'feedcontributions',
+    user: name,
+  };
+  const item = await wikipediaQueryRSS(
+    MEDIAWIKI_ENDPOINT,
+    params,
+    NUMBER_OF_RETRIES
+  );
+  return item;
+};
+/** 
+  SuperFunction for page revisions search
+  Returns Json array of revisions:
+  Each index will contain one revision which will all contain
+  revid: id for the revision that took place
+  parentid: id for the revision that was before it
+  user: user who did the revision
+  timestamp: yyyy-mm-ddT24hrZ format
+  comment: comment on whatever the user decided
+  @Param {string} searchitem - item to query for revisions
+  @returns {Promise} returns revisions if exists otherwise returns -1
+*/
+const pageRevisionsSearch = async searchitem => {
   let item = await getWikibaseItem(searchitem);
   if (item === -1) {
     return -1;
@@ -81,19 +84,23 @@ const wikipediaQuery = async (endpoint, params, n) => {
  * @param {Object} params - Object of parameters to use when querying
  * @param {number} n - Number of times to retry if failure occurs
  * @returns {Promise.<Object>}
-//  */
-// const wikipediaQueryRSS = async (endpoint, params, n) => {
-//   try {
-//     const paramsString = new URLSearchParams(params).toString();
-//     const url = endpoint + '?' + paramsString;
-//     return await Feed.load(url);
-//   } catch (err) {
-//     if (n === 1) {
-//       throw err;
-//     }
-//     return setTimeout(wikipediaQueryRSS(endpoint, params, n - 1), 500);
-//   }
-// };
+ */
+const wikipediaQueryRSS = async (endpoint, params, n) => {
+  try {
+    const paramsString = new URLSearchParams(params).toString();
+    const url1 = endpoint + '?' + paramsString + '&origin=*';
+    return await $.ajax({
+      type: 'GET',
+      url: 'https://api.rss2json.com/v1/api.json?rss_url=' + url1,
+      dataType: 'jsonp',
+    });
+  } catch (err) {
+    if (n === 1) {
+      throw err;
+    }
+    return setTimeout(wikipediaQueryRSS(endpoint, params, n - 1), 500);
+  }
+};
 // Helper function to check whether something is a json string
 // @Param {string} str - string to be checked to see if it is in valid json
 // @returns {boolean} - returns whether the string is in valid json format
@@ -174,7 +181,7 @@ const getRevisionsOfPage = async qid => {
 //Grabs 10 items with pages close to the input text
 // @param {string} searchItem - text to search pages for
 // @returns {Object} - pages in json
-export const getPrefixSearch = async searchItem => {
+const getPrefixSearch = async searchItem => {
   const params = {
     action: 'query',
     format: 'json',
--- src/tests/test.js
+++ src/tests/test.js
@@ -1,8 +1,8 @@
-var test = require('../Backend/searchingFunctionality.js');
+import { pageRevisionsSearch } from '../Backend/searchingFunctionality';
 
 var assert = require('assert');
 describe('Searching for Albert Einstein', function(){
     it('Gets Q957',function(){
-        assert.equal(getWikibaseItem('Albert Einstein'),'Q957');
+        assert.equal(pageRevisionsSearch('Albert Einstein'),'Q957');
     });
 });
