826a4164fd560cc7901b5f66738ed2c27aece085
JeremieSD <jeremie9000@gmail.com>
2021-03-23 01:00:38 +0000

    Starting backend rewrite

--- src/Backend/searchingFunctionality.js
+++ src/Backend/searchingFunctionality.js
@@ -1,49 +1,57 @@
 const WIKIDATA_ENDPOINT = 'https://www.wikidata.org/w/api.php';
 const WIKIPEDIA_ENDPOINT_SEARCH = 'https://en.wikipedia.org/w/api.php';
-const MEDIAWIKI_ENDPOINT = 'https://www.mediawiki.org/w/api.php';
+const DESCRIPTION_REST_API = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
 const NUMBER_OF_RETRIES = 5;
 const fetch = require('node-fetch');
 // var Feed = require('rss-to-json');
 
-//Send you all the information regarding a user
-//Returns whole User Query:
-//title: Think this is useless, havent explored too many of it though but it says that its user contributions and thats about it
-//link: link to user page
-//language: usually repersented like en
-//generator: no idea what this is
-//lastBuildDate:Last time it was queried
-//item: Array of items that the user edited
-//item.title: Title of what the edit was on
-//item.link: Link to the changes
-//item.guid: appears to be the same as link although further exploration is needed
-//item.description: Description of the change, can be used with NLP package to get links
-//item.pubDate: Format = Wed, 24 Feb 2021 01:22:25 GMT
-//item.dc:creator: Returns username of editor to item
-//item.comments: comment of the change
-//@params {name} - User name
-//@returns {json} - regardless of whether the user has done anything or not
-// export const userSearch = async name => {
-//   const params = {
-//     action: 'feedcontributions',
-//     user: name,
-//   };
-//   var item = await wikipediaQueryRSS(
-//     MEDIAWIKI_ENDPOINT,
-//     params,
-//     NUMBER_OF_RETRIES
-//   );
-//   return item;
-// };
-//SuperFunction for page revisions search
-//Returns Json array of revisions:
-//Each index will contain one revision which will all contain
-//revid: id for the revision that took place
-//parentid: id for the revision that was before it
-//user: user who did the revision
-//timestamp: yyyy-mm-ddT24hrZ format
-//comment: comment on whatever the user decided
-//@Param {string} searchitem - item to query for revisions
-//@returns {Promise} returns revisions if exists otherwise returns -1
+/** 
+Send you all the information regarding a user
+Returns whole User Query:
+title: Think this is useless, havent explored too many of it though but it says that its user contributions and thats about it
+link: link to user page
+language: usually repersented like en
+generator: no idea what this is
+lastBuildDate:Last time it was queried
+item: Array of items that the user edited
+item.title: Title of what the edit was on
+item.link: Link to the changes
+item.guid: appears to be the same as link although further exploration is needed
+item.description: Description of the change, can be used with NLP package to get links
+item.pubDate: Format = Wed, 24 Feb 2021 01:22:25 GMT
+item.dc:creator: Returns username of editor to item
+item.comments: comment of the change
+@params {name} - User name
+@returns {Promise} - regardless of whether the user has done anything or not
+*/
+export const userSearch = async name => {
+  const params = {
+    action: 'query',
+    format: 'json',
+    ucuserprefix: 'usercontribs',
+    uclimit: 500,
+    ucuser: name
+  };
+  var item = await wikipediaQuery(
+    WIKIPEDIA_ENDPOINT_SEARCH,
+    params,
+    NUMBER_OF_RETRIES
+  );
+  return item;
+};
+
+/** 
+SuperFunction for page revisions search
+Returns Json array of revisions:
+Each index will contain one revision which will all contain
+revid: id for the revision that took place
+parentid: id for the revision that was before it
+user: user who did the revision
+timestamp: yyyy-mm-ddT24hrZ format
+comment: comment on whatever the user decided
+@Param {string} searchitem - item to query for revisions
+@returns {Promise} returns revisions if exists otherwise returns -1
+*/
 export const pageRevisionsSearch = async searchitem => {
   let item = await getWikibaseItem(searchitem);
   if (item === -1) {
@@ -75,28 +83,13 @@ const wikipediaQuery = async (endpoint, params, n) => {
   }
 };
 
-/**
- * Returns a the response of a query to the Mediawiki API endpoint
- *
- * @param {Object} params - Object of parameters to use when querying
- * @param {number} n - Number of times to retry if failure occurs
- * @returns {Promise.<Object>}
-//  */
-// const wikipediaQueryRSS = async (endpoint, params, n) => {
-//   try {
-//     const paramsString = new URLSearchParams(params).toString();
-//     const url = endpoint + '?' + paramsString + '&origin=*';
-//     return await Feed.load(url);
-//   } catch (err) {
-//     if (n === 1) {
-//       throw err;
-//     }
-//     return setTimeout(wikipediaQueryRSS(endpoint, params, n - 1), 500);
-//   }
-// };
-// Helper function to check whether something is a json string
-// @Param {string} str - string to be checked to see if it is in valid json
-// @returns {boolean} - returns whether the string is in valid json format
+
+
+/** 
+  Helper function to check whether something is a json string
+  @Param {string} str - string to be checked to see if it is in valid json
+  @returns {boolean} - returns whether the string is in valid json format
+*/
 function isJson(str) {
   try {
     JSON.parse(str);
