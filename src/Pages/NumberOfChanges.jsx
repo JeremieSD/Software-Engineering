765290ea74ff062a2f5e2b3bc6bea123fd487afb
aoife-K <khanao@tcd.ie>
2021-03-27 18:42:54 +0000

    fix mistake in last commit

--- src/Pages/NumberOfChanges.jsx
+++ src/Pages/NumberOfChanges.jsx
@@ -59,35 +59,8 @@ export const NumberOfChangesSettings = {
   //     return s;
   //   }, 0);
   // },
-  keys: ['bot', 'human'],
-  index: 'username',
-  xAxis: 'username',
-  yAxis: 'actions',
+  colorBy: 'type',
   colors: 'set2',
-  colorBy: 'id',
-  legend: {
-    dataFrom: 'keys',
-    anchor: 'bottom-right',
-    direction: 'column',
-    justify: false,
-    translateX: 120,
-    translateY: 0,
-    itemsSpacing: 2,
-    itemWidth: 100,
-    itemHeight: 20,
-    itemDirection: 'left-to-right',
-    itemOpacity: 0.85,
-    symbolSize: 20,
-    effects: [
-      {
-        on: 'hover',
-        style: {
-          itemOpacity: 1,
-        },
-      },
-    ],
-  },
-  margin: { top: 5, right: 130, bottom: 50, left: 80 },
   onClick: function(click) {
     window.open('https://www.wikidata.org/wiki/' + click.label, '_blank');
   },
