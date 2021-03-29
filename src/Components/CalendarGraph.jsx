ecc50c62af35ba0dffe915a385746a96da634f28
aoife-K <khanao@tcd.ie>
2021-03-29 13:27:35 +0100

    fix dates on calendar graph

--- src/Components/CalendarGraph.jsx
+++ src/Components/CalendarGraph.jsx
@@ -62,8 +62,8 @@ class CalendarGraph extends Component {
         ) : (
           <ResponsiveCalendar
             data={this.state.data}
-            from="2015-03-01"
-            to="2016-07-12"
+            from="2020-03-01"
+            to="2021-07-12"
             emptyColor="#eeeeee"
             colors={['#61cdbb', '#97e3d5', '#e8c1a0', '#f47560']}
             margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
